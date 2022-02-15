78045a16c001b5efca095c20a7ac8a7653945574
Hazvi Nurafgan <hazvinurafgan@gmail.com>
2022-02-16 01:40:47 +0700

    footer completed header pending slicing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" />
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" />
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp;
