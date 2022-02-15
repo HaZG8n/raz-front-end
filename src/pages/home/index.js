eb1fe0ac7b7d691c55ce98ad0aeb5f9861b05eb2
Fahrul Muhammad <fahrulmuh23@gmail.com>
2022-02-16 01:46:55 +0800

    starting setup

---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import css from "../../commons/styles/home.module.css";
+
+export default class home extends Component {
+  render() {
+    return (
+      <div>
+        <p className={css.text}>hayolo</p>
+        <p className={css.test}>Arimo</p>
+      </div>
+    );
+  }
+}
