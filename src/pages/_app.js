79554db25da2f76e50cd5af2af8ae763f8177a3f
Rahmasbn <rahmasyabani37@gmail.com>
2022-02-17 15:16:18 +0700

    add persist

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,21 @@ import "src/commons/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Script from "next/script";
 import { Provider } from "react-redux";
-import store from "src/redux/store";
+import { store, persistor } from "src/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Provider store={store}>
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" />
-        <Component {...pageProps} />
+        <PersistGate loading={null} persistor={persistor}>
+          <Script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"
+          />
+          <Component {...pageProps} />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,56 @@
 import { createStore, applyMiddleware } from "redux";
-import Logger from "redux-logger";
+import logger from "redux-logger";
 import rpm from "redux-promise-middleware";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import reducers from "./reducers/index";
 
-const saveState = (state) => {
-  try {
-    console.log("masuk load state");
-    const stringifyState = JSON.stringify(state);
-    localStorage.setItem("state", stringifyState);
-  } catch (error) {
-    alert("Theres error from redux store ");
-    console.log(error);
-  }
-};
 
-const loadState = () => {
-  try {
-    const stateFromLocal = localStorage.getItem("state");
-    console.log("masuk load state");
-    if (stateFromLocal === null) {
-      return undefined;
-    } else {
-      return JSON.parse(stateFromLocal);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-};
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  whitelist: ["auth"],      //yang mau dimasukin dlm persist
+}
 
-const theState = loadState();
-const enhancers = applyMiddleware(rpm, Logger);
-const store = createStore(reducers, theState, enhancers);
+const pReducer = persistReducer(persistConfig, reducers);
 
-store.subscribe(() => {
-  saveState(store.getState());
-});
+const enhancers = applyMiddleware(rpm, logger);
+export const store = createStore(pReducer, enhancers);
+export const persistor = persistStore(store);
 
-export default store;
+
+// const saveState = (state) => {
+//   try {
+//     console.log("masuk load state");
+//     const stringifyState = JSON.stringify(state);
+//     localStorage.setItem("state", stringifyState);
+//   } catch (error) {
+//     alert("Theres error from redux store ");
+//     console.log(error);
+//   }
+// };
+
+// const loadState = () => {
+//   try {
+//     const stateFromLocal = localStorage.getItem("state");
+//     console.log("masuk load state");
+//     if (stateFromLocal === null) {
+//       return undefined;
+//     } else {
+//       return JSON.parse(stateFromLocal);
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// const theState = loadState();
+// const enhancers = applyMiddleware(rpm, logger);
+// const store = createStore(reducers, theState, enhancers);
+
+// store.subscribe(() => {
+//   saveState(store.getState());
+// });
+
+// export default store;
