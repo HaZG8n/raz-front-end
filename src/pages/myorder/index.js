c6d27f9b01227e2af5b2a0e318ad953df1447ff6
Fahrul Muhammad <fahrulmuh23@gmail.com>
2022-02-20 23:44:33 +0800

    add my order page

--- src/pages/myorder/index.js
+++ src/pages/myorder/index.js
@@ -1,7 +1,61 @@
 import { Component } from "react";
+import { withRouter } from "next/router";
+import css from "src/commons/styles/myOrder.module.css";
+import { connect } from "react-redux";
+import { getOrder } from "src/commons/module/product";
 
-export default class index extends Component {
+import Main from "src/commons/components/Main";
+import Layout from "src/commons/components/Layout";
+import MenuProfile from "src/commons/components/MenuProfile";
+import Banner from "src/commons/components/Banner/index";
+import CardOrderUser from "src/commons/components/OrderUser";
+
+class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: [],
+    };
+  }
+  async componentDidMount() {
+    try {
+      const token = this.props.token;
+      const result = await getOrder(token);
+      this.setState({ order: result.data.data });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   render() {
-    return <div>index</div>;
+    console.log(this.state.order);
+    return (
+      <>
+        <Layout title="My Order" />
+        <Main>
+          <Banner text="See your notifications for the latest updates" title="My Order" />
+          <MenuProfile />
+          <div className={css.table}>
+            <p>PRODUCT</p>
+            <p>PRICE</p>
+            <p>QUANTITY</p>
+            <p>STATUS ORDER</p>
+            <p>STATUS SHIPING</p>
+            <p>TOTAL</p>
+          </div>
+
+          {this.state.order.map((val) => {
+            return <CardOrderUser shiping={val.shipping_status} price={val.product[0].price} status={val.status_order} quantity={val.quantity} total={val.total_price} name={val.product[0].name} key={val.id} />;
+          })}
+        </Main>
+      </>
+    );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    token: state.auth.token,
+  };
+};
+
+export default withRouter(connect(mapStateToProps)(index));
